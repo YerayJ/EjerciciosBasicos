9c2da3a1d9fb36fa7e9c5908e85d97c3a3b7789e
yjimenez <yjimenez@106-13.info2>
2022-09-21 13:29:04 +0200

    Ejercicios Básicos

---
+++ Exercise8/src/exercise8/Exercise8.java
@@ -0,0 +1,16 @@
+package exercise8;
+
+import java.util.Scanner;
+
+public class Exercise8 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un número: ");
+		int numero= sc.nextInt();
+		
+		System.out.println("Ha escrito "  + numero);
+		
+	}
+
+}
