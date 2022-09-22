caaabea9337ad9f00c5906f8f976cb6123ae6439
YerayJ <114072332+YerayJ@users.noreply.github.com>
2022-09-22 16:41:29 +0200

    Update Exercise16.java

--- Exercise16/src/exercise16/Exercise16.java
+++ Exercise16/src/exercise16/Exercise16.java
@@ -12,25 +12,28 @@ public class Exercise16 {
 	 */
 
 	public static void main(String[] args) {
-		// Declaramos las variables. El valor de euro es lo que vale una peseta
-		double pes;
-		double eur = 0.0066;
+		// Declaramos la variable.
+		Float pes;
 
 		// Creamos un escaner para introducir información
 		Scanner sc = new Scanner(System.in);
-		
-		//Pedimos variable
-		System.out.println("Dame la cantidad en pesetas: ");
-		pes = sc.nextInt();
-		
-		//Creamos un conversor
-		double conv = pes*eur ; 
-		
-		//Imprimimos el resultado por pantalla
-		System.out.print("El resultado de pesetas es: " + conv);
-		
+
+		// Pedimos variable
+		System.out.println("Deme la cantidad en pesetas: ");
+		pes = sc.nextFloat();
+
+		 /*Imprimimos el resultado por pantalla. Lo que hace el print f sería coger un
+		 parámetro en este caso el porcentaje sería el número entero y separamos con
+		 un punto la parte decimal donde indicamos que queremos solo dos caracteres de
+		 tipo float con la f. A continuación le añado a la sintaxis el signo de euro y
+		 por último despúes de la coma se indican los parámetros y multiplico la
+		 variable pasada por el usuario por el valor de una peseta en euros
+		 */
+		 
+		System.out.printf("El resultado de pesetas es: %.2f" + "€", (pes * 0.006));
+
 		sc.close();
-		
+
 	}
 
 }
