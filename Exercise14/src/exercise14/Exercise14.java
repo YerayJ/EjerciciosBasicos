430b80a4043028ce4b2e00a081cda82173a12937
YerayJ <114072332+YerayJ@users.noreply.github.com>
2022-09-22 14:02:19 +0200

    Add files via upload

---
+++ Exercise14/src/exercise14/Exercise14.java
@@ -0,0 +1,35 @@
+package exercise14;
+
+import java.util.Scanner;
+
+public class Exercise14 {
+
+	/*
+	 * Escribir un programa que le pida al usuario su nombre, dirección y teléfono.
+	 * Guarda cada dato en variables distintas. A continuación, muestra los datos de
+	 * la siguiente forma (en rojo serían los datos introducidos por el usuario):
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		// Creamos un escaner para introducir información
+		Scanner sc = new Scanner(System.in);
+	
+		// Pedimos los datos de la persona en este caso también usamos String
+		
+		System.out.println("Deme su nombre: ");
+		String nom = sc.nextLine();
+		System.out.println("Deme su dirección: ");
+		String dir = sc.nextLine();
+		System.out.println("Deme su télefono: ");
+		int tlf = sc.nextInt();
+		
+		//Imprimimos en pantalla los datos 
+		
+		System.out.println("Nombre: " + nom);
+		System.out.println("Dirección: " + dir);
+		System.out.println("Teléfono: " + tlf);
+
+	}
+
+}
